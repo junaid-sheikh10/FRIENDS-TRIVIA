b55d62c93704c7407cb26701a0fe285b50f14600
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2021-09-30 12:12:28 +0000

    changed questions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var readlineSync=require('readline-sync');
 var name=readlineSync.question("whats your name?")
 console.log("Hey "+name+" !")
-console.log("Welcome to 'DO YOU KNOW JUNAID' ")
+console.log("Welcome to FRIENDS Trivia ")
 console.log("********************************")
 
 //var question1="what's my favourite color?";
@@ -17,7 +17,7 @@ var highScores = [
   },
 
   {
-    name: "Atharv2",
+    name: "alina",
     score: 2,
   },
 ]
@@ -37,10 +37,10 @@ function play(que,ans){
   console.log("-----------------")
 }
 
-play("Which colour does Junaid wear the most?","blue")
-play("What is Junaid's work location","bangalore")
-play("What is Junaid's weight? \n(PS-it ranges from 57-61)","60")
-play("what is Junaid's 2nd favourite sport ?","badminton")
+play("How many sisters does Joey have?","7")
+play("Who hates Thanksgiving","chandler")
+play("What is the name of Phoebe's most popular song","Smelly Cats")
+play("Who says the last line of the series ?","Chandler")
 
 console.log("Thanks for playing the game "+name)
 console.log("Your final score is "+score)
@@ -48,3 +48,5 @@ console.log("Your final score is "+score)
 console.log("Check out the high scores, if you should be there ping me and I'll update it");
 
   highScores.map(score => console.log(score.name, " : ", score.score))
+
+  
